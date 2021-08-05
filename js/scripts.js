01a4411bc94d819ea3dddabc2d2452b955679150
IamVoNha <ov_ahn@yahoo.com>
2021-08-05 10:49:40 -0400

    added cross out list item function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function newItem() {
-  //Adding a new item to the list of items:
+  //Adding a new item to the list of items!
   let listItem = $('<li></li>');
   let inputValue = $('#input').val();
       listItem.append(inputValue);
@@ -9,3 +9,8 @@ function newItem() {
     } else {
       $('#list').append(listItem);
     }
+
+  //crossing out item in list!
+  listItem.on('dbclick',function crossOut() {
+    listItem.toggleClass('strike');
+  });
